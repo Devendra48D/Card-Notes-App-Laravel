0d9c58f9029d84c0e12e59e6cac19abc67495822
Devendra Shah <devendrashah97@gmail.com>
2016-06-16 00:04:55 -0400

    Edit and Delete buttons added

--- my-project/resources/views/cards/show.blade.php
+++ my-project/resources/views/cards/show.blade.php
@@ -3,15 +3,6 @@
 
 @section('content')
 
-<div class="row">
-	<div class="col-md-6 col-md-offset-3">
-	<h1>{{$card->title}}</h1>
-<ul class="list-group">
-	@foreach ($card->notes as $note)
-		<li class = "list-group-item">{{$note->body}}</li>
-	@endforeach
-</ul>
-<hr>
 <h3 id="action-header">Add a new note</h3>
 
 <form method="POST" action="/cards/{{ $card->id }}/notes">
@@ -26,35 +17,41 @@
 </div>
 </form>
 
-<form method="POST" action="/cards/{{ $card->id }}/notes">
-{{ csrf_field() }}
-
-
 <hr>
-<h3>Delete a note</h3>
-<form method="DELETE" action="/cards/{{ $card->id }}/updatednotes">
-{{ csrf_field() }}
-	<div class="form-group">
-	<textarea name="body" class="form-control"></textarea>
-</div>
 
-	<div class="form-group">
-	<button type="submit" class="btn btn-primary">Delete note</textarea>
-</div>
-</form>
+
+<div class="row">
+	<div class="col-md-6 col-md-offset-3">
+	<h1>{{$card->title}}</h1>
+<ul class="list-group">
+	@foreach ($card->notes as $note)
+		<li class = "list-group-item">
+			{{$note->body}} 
+			<button type="button" class="btn btn-default">
+  <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
+</button>
+
+			<button type="button" class="btn btn-default btn-lg">
+  <span class="glyphicon glyphicon-trash" aria-hidden="true"></span>
+</button>
+		</li>
+	@endforeach
+</ul>
+
+
+
 </div>
 </div>
 @endsection 
 @section('script')
 	<script>
-	        function changeActionHeader()
+	    public function changeActionHeader()
     	{
     		var header = document.getElementById("action-header");
     		alert (header.innerText);
     		header.innerText="Edit text";
 
     	}
-    	alert('Hi');
     	window.changeActionHeader();
     	</script>
 @endsection
